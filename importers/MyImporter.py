b1bdcd38b3e4ee883c06a15a4700e4512c75ea96
misska <michaela.jandekova@gmail.com>
2015-09-08 11:20:34 +0200

    typo

--- importers/MyImporter.py
+++ importers/MyImporter.py
@@ -8,8 +8,8 @@ class MyImporter(Importer):
     url = 'http://www.mystores.cz/department-1396.html'
 
     def parse(self):
-        for departments in self.soup.findAll("section", {"class":"departments"}):
-            for shopList in departments.findAll("ul"):
+        for department in self.soup.findAll("section", {"class":"departments"}):
+            for shopList in department.findAll("ul"):
                 for link in shopList.findAll("a"):
                     if isinstance(link.text, basestring):
                         self.shops.append(link.text)
